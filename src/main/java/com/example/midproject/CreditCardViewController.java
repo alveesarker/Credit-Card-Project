aad2d1e3174420a32139861c071de2d38b14b19d
alveesarker <asahanaz305@gmail.com>
2025-03-03 15:21:28 +0600

    completed the project

--- src/main/java/com/example/midproject/CreditCardViewController.java
+++ src/main/java/com/example/midproject/CreditCardViewController.java
@@ -1,10 +1,13 @@
 package com.example.midproject;
 
-import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
 
-public class CreditCardViewController
-{
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreditCardViewController {
     @javafx.fxml.FXML
     private TableColumn<CreditCard, String> gatewayColumn;
     @javafx.fxml.FXML
@@ -32,19 +35,97 @@ public class CreditCardViewController
     @javafx.fxml.FXML
     private TextField filteredCardLimitTextField;
 
+    private final List<CreditCard> creditCardList = new ArrayList<>();
+    @javafx.fxml.FXML
+    private Text averageCreditLimitText;
+
     @javafx.fxml.FXML
     public void initialize() {
+        gatewayCombobox.getItems().addAll("Visa", "MasterCard");
+        filteredGatewayCombobox.getItems().addAll("All", "Visa", "MasterCard");
+        cardTypeCombobox.getItems().addAll("silver", "gold", "platinum", "titanium");
+
+        cardNumberColumn.setCellValueFactory(new PropertyValueFactory<>("cardNo"));
+        gatewayColumn.setCellValueFactory(new PropertyValueFactory<>("gatewayName"));
+        holderNameColumn.setCellValueFactory(new PropertyValueFactory<>("holderName"));
+        creditLimitColumn.setCellValueFactory(new PropertyValueFactory<>("creditLimit"));
+    }
+
+
+    //CreditCard(String cardNo, String holderName, LocalDate dateOfExpire,
+    // String cardType, String gatewayName, float creditLimit)
+    @javafx.fxml.FXML
+    public void addCardToListButtonOnclick() {
+        String cardNumber = cardNumberTextField.getText();
+        String gateWay = gatewayCombobox.getValue();
+        if (validateCardNumber(cardNumber, gateWay)) {
+
+            creditCardList.add(
+                    new CreditCard(
+                            cardNumberTextField.getText(),
+                            holderNameTextField.getText(),
+                            expireDatePicker.getValue(),
+                            cardTypeCombobox.getValue(),
+                            gatewayCombobox.getValue(),
+                            Float.parseFloat(cardLimitTextField.getText())
+                    )
+            );
+        }
     }
 
     @javafx.fxml.FXML
-    public void addCardToListButtonOnclick(ActionEvent actionEvent) {
+    public void searchAndLoadTableOnAction() {
+        cardTableView.getItems().clear(); // Clear previous data
+        String gateWay = filteredGatewayCombobox.getValue();
+        float minLimit = Float.parseFloat(filteredCardLimitTextField.getText());
+
+
+        // Manually add each CreditCard to the TableView
+        if (gateWay.equals("All")) {
+            for (CreditCard card : creditCardList) {
+                if (minLimit <= card.getCreditLimit()) {
+                    cardTableView.getItems().add(card);
+                }
+            }
+        } else {
+            for (CreditCard card : creditCardList) {
+                if (gateWay.equals(card.getGatewayName()) && minLimit <= card.getCreditLimit()) {
+                    cardTableView.getItems().add(card);
+                }
+            }
+        }
+    }
+
+    private boolean validateCardNumber(String cardNo, String gateWay) {
+        if (cardNo == null || cardNo.length() != 16) {
+            showAlert("Invalid card number! Card number must contains 16 digits.");
+            return false;
+        }
+
+        if ((gateWay.equals("Visa") && !cardNo.startsWith("4")) || (gateWay.equals("MasterCard") && !cardNo.startsWith("5"))) {
+            showAlert("Invalid card number!");
+            return false;
+        }
+
+        return true;
     }
 
     @javafx.fxml.FXML
-    public void searchAndLoadTableOnAction(ActionEvent actionEvent) {
+    public void CreditAverageAndLoadTableOnAction() {
+        List<CreditCard> tableViewItems = cardTableView.getItems();
+        float total = 0;
+        for (CreditCard c : tableViewItems) {
+            total += c.getCreditLimit();
+        }
+        float average = total / tableViewItems.size();
+        String totalString = Float.toString(average);
+        averageCreditLimitText.setText(totalString);
     }
 
-    @javafx.fxml.FXML
-    public void CreditAverageAndLoadTableOnAction(ActionEvent actionEvent) {
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
+
--- src/main/java/com/example/midproject/MainApplicationClass.java
+++ src/main/java/com/example/midproject/MainApplicationClass.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplicationClass extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("credit-card-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplicationClass.class.getResource("credit-card-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("CreditCard");
         stage.setScene(scene);
         stage.show();
     }
